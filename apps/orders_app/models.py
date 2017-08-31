286497e06abaff3bacb6131012a547a6d98ee432
Gisel Calvo <gisel.calvo@gmail.com>
2017-08-31 16:07:57 -0700

    temp

--- apps/orders_app/models.py
+++ apps/orders_app/models.py
@@ -1,10 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
-# from models import User, Product
+from EcommerceSite.users_app.models import User
+from EcommerceSite.products_app.models import Product
 
 
-# # Create your models here.
-# class Order(models.Model):
-# 	#products = models.ForeignKey(Product, related_name = "ordered_product")
-# 	user = models.ForeignKey(User, related_name ="users_order")
+# Create your models here.
+class Order(models.Model):
+	#products = models.ForeignKey(Product, related_name = "ordered_product")
+	#user = models.ForeignKey(User, related_name ="users_order")
+	pass
