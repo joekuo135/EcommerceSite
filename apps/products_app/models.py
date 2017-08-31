8414f4f49d9706ece06e434c8a60265ae97948ef
Joe Kuo <bluehawk135@gmail.com>
2017-08-31 13:30:16 -0700

    updates

--- apps/products_app/models.py
+++ apps/products_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
 	name = models.CharField(max_length=255) 
-	category = models.CharField(max_length=255, null=True) #Mens, Womens, Kids,
+	category = models.CharField(max_length=255, null=True) #Mens, Womens, Kids, Accessories
 	subCategory = models.CharField(max_length=255, null=True) #Apperal, Footwear, Jewelry, Watches
 	description = models.TextField()
 	image1 = models.CharField(max_length=255)
